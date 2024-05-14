55a5c1ff75d4fde206a9c22d4453ebe578ac3b2b
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2024-05-14 12:12:45 +0200

    feat(pcm-test): implement connector uncertainty test case

---
+++ tests/dev.abunai.confidentiality.analysis.tests/src/dev/abunai/confidentiality/analysis/tests/pcm/ConnectorUncertaintyTest.java
@@ -0,0 +1,63 @@
+package dev.abunai.confidentiality.analysis.tests.pcm;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import dev.abunai.confidentiality.analysis.core.UncertainConstraintViolation;
+import dev.abunai.confidentiality.analysis.pcm.PCMUncertainFlowGraphCollection;
+import dev.abunai.confidentiality.analysis.pcm.PCMUncertainTransposeFlowGraph;
+import dev.abunai.confidentiality.analysis.tests.PCMTestBase;
+import org.dataflowanalysis.analysis.core.CharacteristicValue;
+import org.dataflowanalysis.analysis.pcm.core.seff.SEFFPCMVertex;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ConnectorUncertaintyTest extends PCMTestBase {
+    @Override
+    protected String getFolderName() {
+        return "PCMConnectorUncertainty";
+    }
+
+    @Override
+    protected String getFilesName() {
+        return "default";
+    }
+
+    @Test
+    public void testResults() {
+        PCMUncertainFlowGraphCollection flowGraphs = (PCMUncertainFlowGraphCollection) analysis.findFlowGraph();
+        PCMUncertainFlowGraphCollection uncertainFlowGraphs = flowGraphs.createUncertainFlows();
+        uncertainFlowGraphs.evaluate();
+
+        List<UncertainConstraintViolation> result = analysis.queryUncertainDataFlow(uncertainFlowGraphs, node -> {
+            List<String> serverLocation = node.getVertexCharacteristics("ServerLocation")
+                    .stream()
+                    .map(CharacteristicValue::getValueName)
+                    .toList();
+            List<String> dataSensitivity = node.getDataCharacteristicMap("DataSensitivity")
+                    .values()
+                    .stream()
+                    .flatMap(Collection::stream)
+                    .map(CharacteristicValue::getValueName)
+                    .toList();
+            List<String> encryption = node.getDataCharacteristicMap("Encryption")
+                    .values()
+                    .stream()
+                    .flatMap(Collection::stream)
+                    .map(CharacteristicValue::getValueName)
+                    .toList();
+
+            return dataSensitivity.stream()
+                    .anyMatch(l -> l.equals("Personal"))
+                    && serverLocation.stream()
+                    .anyMatch(l -> l.equals("nonEU"))
+                    && encryption.stream().noneMatch(l -> l.equals("Encrypted"));
+        });
+
+        assertEquals(1, result.size());
+        assertEquals(3, result.get(0).violations().size());
+        assertEquals("StoreUserDataCall", ((SEFFPCMVertex<?>)((PCMUncertainTransposeFlowGraph) result.get(0).transposeFlowGraph()).getVertices().get(14)).getReferencedElement().getEntityName());
+    }
+}
