ea3c4877d9c43c91189cf0bee1ef92a451252922
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2024-08-13 15:13:05 +0200

    feat(evaluation): fix distance tracker model

--- tests/dev.abunai.confidentiality.analysis.tests/src/dev/abunai/confidentiality/analysis/tests/pcm/DistanceTrackerTest.java
+++ tests/dev.abunai.confidentiality.analysis.tests/src/dev/abunai/confidentiality/analysis/tests/pcm/DistanceTrackerTest.java
@@ -38,32 +38,13 @@ public class DistanceTrackerTest extends PCMTestBase {
 		
 		uncertainFlowGraphs.evaluate();
 
-		/*
 		List<UncertainConstraintViolation> accessViolations = analysis.queryUncertainDataFlow(uncertainFlowGraphs, node -> {
-			List<String> ownedRoles = node.getVertexCharacteristics("OwnedRoles")
-	                .stream()
-	                .map(CharacteristicValue::getValueName)
-	                .toList();
-	        Collection<List<CharacteristicValue>> allowedRoles = node.getDataCharacteristicMap("AllowedRoles")
-	                .values();
-
-	        for (List<CharacteristicValue> dataFlowCharacteristics : allowedRoles) {
-	            if (!dataFlowCharacteristics.isEmpty() && dataFlowCharacteristics.stream()
-	                    .distinct()
-	                    .map(CharacteristicValue::getValueName)
-	                    .noneMatch(ownedRoles::contains)) {
-	                return true;
-	            }
-	        }
-	        return false;
+	        List<String> ownedRoles = node.getVertexCharacteristicNames("OwnedRoles");
+	        Collection<List<String>> allowedRoles = node.getDataCharacteristicNamesMap("AllowedRoles").values();
+			return !ownedRoles.contains("User") 
+	        		&& !allowedRoles.stream().allMatch(var -> var.stream().anyMatch(role -> ownedRoles.contains(role)));
 		});
 		assertTrue(accessViolations.size() > 0);
-		*/
-		
-		List<UncertainConstraintViolation> leaks = analysis.queryUncertainDataFlow(uncertainFlowGraphs, it -> {
-			return it.getDataCharacteristicNamesMap("DataOrigin").values().stream().anyMatch(vars -> vars.stream().anyMatch(cv -> cv.equals("Leaked")));
-		});
-		assertTrue(leaks.size() > 0);
 	}
 
 }
