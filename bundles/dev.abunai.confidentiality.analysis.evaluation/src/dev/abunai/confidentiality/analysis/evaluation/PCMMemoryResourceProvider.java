ce320b1e665853f26c8456ab7e117fafcecbd8e3
Felix Schwickerath <felix.schwickerath@student.kit.edu>
2024-09-09 17:07:23 +0200

    feat(evaluation): fix merge conflicts

---
+++ bundles/dev.abunai.confidentiality.analysis.evaluation/src/dev/abunai/confidentiality/analysis/evaluation/PCMMemoryResourceProvider.java
@@ -0,0 +1,57 @@
+package dev.abunai.confidentiality.analysis.evaluation;
+
+import java.util.List;
+
+import dev.abunai.confidentiality.analysis.model.uncertainty.UncertaintyPackage;
+import dev.abunai.confidentiality.analysis.model.uncertainty.UncertaintySourceCollection;
+import dev.abunai.confidentiality.analysis.pcm.PCMUncertaintyResourceProvider;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.palladiosimulator.pcm.allocation.Allocation;
+import org.palladiosimulator.pcm.allocation.AllocationPackage;
+import org.palladiosimulator.pcm.usagemodel.UsageModel;
+import org.palladiosimulator.pcm.usagemodel.UsagemodelPackage;
+
+public class PCMMemoryResourceProvider extends PCMUncertaintyResourceProvider {
+    public PCMMemoryResourceProvider(List<Resource> resources) {
+        super(null, null, null, null);
+        this.resources.getResources().addAll(resources);
+    }
+
+    @Override
+    public UsageModel getUsageModel() {
+        for (Resource resource : this.resources.getResources()) {
+            if (this.isTargetInResource(UsagemodelPackage.eINSTANCE.getUsageModel(),
+                    resource)) {
+                return (UsageModel) resource.getContents().get(0);
+            }
+        }
+        throw new IllegalStateException("Resources do not contain a usage model");
+    }
+
+    @Override
+    public Allocation getAllocation() {
+        for (Resource resource : this.resources.getResources()) {
+            if (this.isTargetInResource(AllocationPackage.eINSTANCE.getAllocation(),
+                    resource)) {
+                return (Allocation) resource.getContents().get(0);
+            }
+        }
+        throw new IllegalStateException("Resources do not contain a allocation");
+    }
+
+    @Override
+    public UncertaintySourceCollection getUncertaintySourceCollection() {
+        for (Resource resource : this.resources.getResources()) {
+            if (this.isTargetInResource(UncertaintyPackage.eINSTANCE.getUncertaintySourceCollection(),
+                    resource)) {
+                return (UncertaintySourceCollection) resource.getContents().get(0);
+            }
+        }
+        throw new IllegalStateException("Resources do not contain a uncertainty source collection");
+    }
+
+    @Override
+    public void loadRequiredResources() {
+
+    }
+}
---
+++ bundles/dev.abunai.confidentiality.analysis.evaluation/src/dev/abunai/confidentiality/analysis/evaluation/factory/builder/InterfaceBuilder.java
@@ -0,0 +1,73 @@
+package dev.abunai.confidentiality.analysis.evaluation.factory.builder;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.UUID;
+
+import org.palladiosimulator.pcm.repository.DataType;
+import org.palladiosimulator.pcm.repository.OperationInterface;
+import org.palladiosimulator.pcm.repository.OperationSignature;
+import org.palladiosimulator.pcm.repository.Parameter;
+import org.palladiosimulator.pcm.repository.Repository;
+import org.palladiosimulator.pcm.repository.RepositoryFactory;
+
+public class InterfaceBuilder {
+	private Repository repository;
+	private OperationInterface operationInterface;
+	
+	
+	private InterfaceBuilder(Repository repository) {
+		this.repository = repository;
+		this.operationInterface = RepositoryFactory.eINSTANCE.createOperationInterface();
+		this.operationInterface.setId(UUID.randomUUID().toString());
+		this.operationInterface.setRepository__Interface(repository);
+		this.repository.getInterfaces__Repository().add(operationInterface);
+	}
+
+	
+	public static InterfaceBuilder builder(Repository repository) {
+		return new InterfaceBuilder(repository);
+	}
+	
+	public InterfaceBuilder setName(String name) {
+		operationInterface.setEntityName(name);
+		return this;
+	}
+	
+	public OperationSignature addOperation(String name) {
+		OperationSignature signature = RepositoryFactory.eINSTANCE.createOperationSignature();
+		signature.setId(UUID.randomUUID().toString());
+		signature.setEntityName(name);
+		signature.setInterface__OperationSignature(this.operationInterface);
+		return signature;
+	}
+	
+	public OperationSignature addOperation(String name, Map<String, DataType> parameterData) {
+		OperationSignature signature = RepositoryFactory.eINSTANCE.createOperationSignature();
+		signature.setId(UUID.randomUUID().toString());
+		signature.setEntityName(name);
+		signature.setInterface__OperationSignature(operationInterface);
+		for(Entry<String, DataType> entry : parameterData.entrySet()) {
+			Parameter param =  RepositoryFactory.eINSTANCE.createParameter();
+			param.setParameterName(entry.getKey());
+			param.setDataType__Parameter(entry.getValue());
+			signature.getParameters__OperationSignature().add(param);
+		}
+		return signature;
+	}
+	
+	public OperationSignature addOperation(String name, List<Parameter> parameter, DataType returnType) {
+		OperationSignature signature = RepositoryFactory.eINSTANCE.createOperationSignature();
+		signature.setId(UUID.randomUUID().toString());
+		signature.setEntityName(name);
+		signature.setInterface__OperationSignature(operationInterface);
+		signature.getParameters__OperationSignature().addAll(parameter);
+		signature.setReturnType__OperationSignature(returnType);
+		return signature;
+	}
+	
+	public OperationInterface build() {
+		return this.operationInterface;
+	}
+}
